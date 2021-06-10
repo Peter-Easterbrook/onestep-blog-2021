d9bdf8b3c7b2287ce8e665d7d1c21892b1178bfc
Peter-Easterbrook <peter.easterbrook@gmail.com>
2021-06-10 11:29:26 +0200

    Added NextJS Blog

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,17 @@ export default function aboutPage() {
       </h1>
       <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
         <h3 className='text-2xl mb-5 '>OneStep Blog</h3>
-        <p className='mb-3'>This blog is built with Next.js and Markdown</p>
+        <p className='mb-3'>This blog is built with: </p>
+        <ul>
+          <li>NextJS</li>
+          <li>Markdown</li>
+          <li>ReactJS</li>
+          <li>Gray-Matter</li>
+          <li>Marked</li>
+          <li>Husky</li>
+          <li>PostCSS</li>
+          <li>Tailwind</li>
+        </ul>
         <p className='font-medium'> Version 1.0.0</p>
       </div>
     </Layout>
