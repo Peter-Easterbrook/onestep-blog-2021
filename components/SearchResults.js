ed9406c613ec5464ac61a9dd1dd961282ce41cdb
Peter-Easterbrook <peter.easterbrook@gmail.com>
2021-06-21 13:27:23 +0200

    Fixed search popup padding issue

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,8 +4,8 @@ export default function SearchResults({ results }) {
   if (results.length === 0) return <></>;
 
   return (
-    <div className='absolute top-20 right-0 md:right-10 z-10  border-2 border-gray-500 bg-white text-black w-full rounded-2xl'>
-      <div className='p-6'>
+    <div className='absolute top-20 right-0 md:right-10 z-10 border-2 border-gray-500 bg-white text-black w-full rounded-2xl'>
+      <div className='py-4 px-4'>
         <h2 className='text-2xl mb-3'>{results.length} Results</h2>
         {results.map((result, index) => (
           <Post key={index} post={result} compact={true} />
