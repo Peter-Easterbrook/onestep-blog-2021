6b59d6403f0500c1d6f69bfb69bbcd32b103077d
Peter-Easterbrook <peter.easterbrook@gmail.com>
2021-09-07 17:34:01 +0200

    Reduces 'about' size

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,10 @@ import Layout from '@/components/Layout';
 export default function aboutPage() {
   return (
     <Layout title='About OneStep'>
-      <h1 className='text-4xl p-5 font-normal text-gray-100 bg-gray-900 border border-gray-500 rounded-lg text-center'>
+      <h1 className='text-4xl p-5 font-normal text-gray-100 bg-gray-900 border border-gray-500 rounded-lg text-center mx-80'>
         About
       </h1>
-      <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
+      <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6 mx-80'>
         <h3 className='text-2xl mb-5 '>OneStep Blog</h3>
         <p className='mb-3'>This blog is built with: </p>
         <ul>
