248e932731303941e7aca68eadb90208a0ebe992
Peter <peter.easterbrook@gmail.com>
2025-12-25 09:35:34 +0100

    refactor: update image imports to use 'next/image' and adjust Tailwind CSS imports

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,4 @@
-import Link from 'next/link';
-import Image from 'next/legacy/image';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
 
 export default function NotFoundPage() {
